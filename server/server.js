22b848df0c235d28352869b12795dffdf37e9bc6
Louie Tolentino <silvercloud.0503@gmail.com>
2023-04-01 06:55:56 +0000

    added: KPI Schema and insert mock data.

--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import kpiRoutes from "./routes/kpi.js";
+import KPI from "./models/KPI.js";
+import { kpis } from "./data/data.js"
 
 /* SERVER CONFIGURATION */
 dotenv.config();
@@ -17,6 +20,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+/* API ROUTES */
+app.use("/kpi", kpiRoutes);
+
 /* MONGOOSE CONFIGURATION */
 const PORT = process.env.SERVER_PORT || 9000;
 mongoose
@@ -26,5 +32,10 @@ mongoose
   })
   .then(async () => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  
+  // RUN ONLY ONCE
+  // await mongoose.connection.db.dropDatabase();
+  // KPI.insertMany(kpis)
+
   })
   .catch((error) => console.log(`${error} did not connect`));
