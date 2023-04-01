81779d0a001df7dffdf08ff776dc72438eb69a88
SILV3RCLOUD <silvercloud.0503@gmail.com>
2023-04-01 09:21:27 +0800

    added: Navbar and FlexBetween component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,27 @@
-import { CssBaseline } from "@mui/material";
+import { Box, CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles"
 import { useMemo } from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeSettings } from "./theme"
+import Navbar from "@/scenes/navbar";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
-
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>Dashboard page</div>} />
+              <Route path="/predictions" element={<div>Predictions Page</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ client/src/components/FlexBetween.tsx
@@ -0,0 +1,11 @@
+import { defaultListboxReducer } from "@mui/base";
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+const FlexBetween = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center"
+});
+
+export default FlexBetween;
