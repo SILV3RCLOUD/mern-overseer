c30d37a5227f0e00163a0975fef24d87dcd81926
SILV3RCLOUD <silvercloud.0503@gmail.com>
2023-04-03 16:56:08 +0800

    update: Data variable correction

--- client/src/scenes/dashboard/Row3.tsx
+++ client/src/scenes/dashboard/Row3.tsx
@@ -10,7 +10,7 @@ import { GridCellParams } from '@mui/x-data-grid/models'
 const Row3 = ( ) => {
   const { palette } = useTheme();
   const { data: kpiData } = useGetKpisQuery();
-  const { data: transactionsData } = useGetTransactionsQuery();
+  const { data: transactionData } = useGetTransactionsQuery();
   const { data: productData } = useGetProductsQuery();
   
   const productColumns = [
@@ -30,6 +30,31 @@ const Row3 = ( ) => {
       headerName: "Price",
       flex: 0.5,
       renderCell: (params: GridCellParams) => `PHP${params.value}`,
+    }
+  ]
+
+  const transactionColumns = [
+    {
+      field: "_id",
+      headerName: "id",
+      flex: 1,
+    },
+    {
+      field: "buyer",
+      headerName: "Buyer",
+      flex: 0.65,
+    },
+    {
+      field: "amount",
+      headerName: "Amount",
+      flex: 0.35,
+      renderCell: (params: GridCellParams) => `PHP${params.value}`,
+    },
+    {
+      field: "productIds",
+      headerName: "Count",
+      flex: 0.1,
+      renderCell: (params: GridCellParams) => (params.value as Array<String>).length,
     }
   ]
 
@@ -63,8 +88,35 @@ const Row3 = ( ) => {
         </Box>
         
       </DashboardBox>
-      <DashboardBox gridArea="h"></DashboardBox>
-      <DashboardBox gridArea="i"></DashboardBox>
+      <DashboardBox gridArea="h">
+      <BoxHeader title="List of Recent Orders" sideText={`${transactionData?.length} latest transactions`} />
+        <Box mt="1rem" p="0 0.5rem" height="80%" sx={{
+          "& .MuiDataGrid-root": {
+            color: palette.grey[300],
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: `1px solid ${palette.grey[800]} !important`,
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            borderBottom: `1px solid ${palette.grey[800]} !important`,
+          },
+          "& .MuiDataGrid-columnSeperators": {
+            visibility: "hidden"
+          }
+        }}>
+          <DataGrid 
+            columnHeaderHeight={25}
+            rowHeight={35}
+            hideFooter={true}
+            rows={transactionData || []}
+            columns={transactionColumns}
+          />
+          </Box>
+      </DashboardBox>
+      <DashboardBox gridArea="i">
+        
+      </DashboardBox>
       <DashboardBox gridArea="j"></DashboardBox>
     </>
   )
